6d88201ec390f0aa8fbf46982f0fed6b86bc87db
Eugene-Eyram-Ahiavey <phostechservices@gmail.com>
2025-09-18 11:04:16 +0000

    move orders page to React

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-
+import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage.jsx";
-import '/index.css';
+import "/index.css";
+import { CheckoutPage } from "./pages/CheckoutPage.jsx";
+import { OrdersPage } from "./pages/OrdersPage.jsx";
+import { TrackingPage } from "./pages/TrackingPage.jsx";
 
-function App(){
-
+function App() {
   return (
     <>
-    <HomePage/>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path='orders' element={<OrdersPage/>}/>
+      <Route path='tracking' element={<TrackingPage/>} />
+    </Routes>
     </>
-  )
+  );
 }
 
 export default App;

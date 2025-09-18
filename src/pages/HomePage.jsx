a04b13db8389a25e72c7fc7395baae14dce06696
Eugene-Eyram-Ahiavey <phostechservices@gmail.com>
2025-09-18 21:39:37 +0000

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,6 +6,13 @@ import { products } from '../starting-code/data/products.js';
 
 export function HomePage(){
 
+  fetch('http://localhost:3000/api/products')
+  .then((response) => {
+   return response.json();
+  }).then((data) => {
+    console.log(data);
+  })
+
     return (
     <>
  <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />

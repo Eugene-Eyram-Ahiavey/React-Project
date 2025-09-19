f694290358aed11075ea9e8b706a2278f3168c51
Eugene-Eyram-Ahiavey <phostechservices@gmail.com>
2025-09-19 22:00:24 +0000

    Get cart from the backend and display quantity

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,12 +7,19 @@ import { Header } from '../components/Header.jsx';import './HomePage.css';
 
 export function HomePage(){
 const [products, setProducts] = useState([]);
+const [cart, setCart] = useState([]);
 
 useEffect(() => {
 axios.get('http://localhost:3000/api/products')
 .then((response) => {
-  setProducts(response.data);
+  setProducts(response.data); 
 });
+
+
+axios.get('http://localhost:3000/api/cart-items')
+  .then((response) => {
+    setCart(response.data);
+  })
 }, []);
 
   
@@ -23,7 +30,7 @@ axios.get('http://localhost:3000/api/products')
 
     <title>HomePage</title>
     
-    <Header/>
+    <Header cart={cart}/>
     <div className="home-page">
       <div className="products-grid">
         {products.map((product)=>{
